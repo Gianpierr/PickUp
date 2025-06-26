6df6958caa04af65947d5f1b4b9d62d11a0b3a20
Sydney Wells <smwells06@gmail.com>
2025-06-25 21:19:58 -0500

    modified pages

--- pickup_frontend/src/pages/login.jsx
+++ pickup_frontend/src/pages/login.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { TextField, Button, Container, Typography, Box } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import MuiLink from '@mui/material/Link';
+import sportsBg from '../assets/sports-bg.jpg';
 
 
 function Login() {
@@ -11,25 +14,36 @@ const handleLogin = (event) => {
 
   return (
     <div
-      style={{
-        minHeight: '75vh',
+        style={{
+        minHeight: '100vh',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        background: '#f5f5f5', // Optional: add a background color or image
-      }}
-      >
-        <Container maxWidth="xs" sx={{ backgroundColor: 'grey', padding: 3, borderRadius: 2, boxShadow: 3 }}>
+        backgroundImage: `url(${sportsBg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        }}
+        >
+        <Container maxWidth="xs" sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2, boxShadow: 3 }}>
         <Box sx={{ mt: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Typography component="h1" variant="h5">
-            Login
+            <Typography 
+                component="h1"
+                variant="h5"
+                align="center" // Center the text
+                sx={{ width: '100%' }} // Ensure it takes full width for centering
+                >            
+                Welcome to the Pickup Game App! <br /> 
+                Login
             </Typography>
-            <Box component="form" sx={{ mt: 1 }}>
+            <Box component="form" sx={{ mt: 1 }} onSubmit={handleLogin}>
             <TextField margin="normal" required fullWidth label="Email Address" name="email" autoComplete="email" autoFocus />
             <TextField margin="normal" required fullWidth name="password" label="Password" type="password" autoComplete="current-password" />
-            <Button onClick={handleLogin}>
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 2 }}>
                 Sign In
             </Button>
+            <MuiLink component={RouterLink} to="/signup" variant="body2" sx={{ display: 'block', textAlign: 'center', mt: 1 }}>
+              Don't have an account? Sign Up
+            </MuiLink>
             </Box>
         </Box>
         </Container>
--- pickup_frontend/src/pages/signup.jsx
+++ pickup_frontend/src/pages/signup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, Button, Container, Typography, Box } from '@mui/material';
+import { TextField, Button, Container, Typography, Box, MenuItem } from '@mui/material';
 
 function SignUp() {
   return (
@@ -9,7 +9,32 @@ function SignUp() {
           Sign Up
         </Typography>
         <Box component="form" sx={{ mt: 1 }}>
-          <TextField margin="normal" required fullWidth label="Username" name="username" autoFocus />
+          <TextField margin="normal" required fullWidth label="First Name" name="firstName" autoFocus />
+          <TextField margin="normal" required fullWidth label="Last Name" name="lastName" />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Birthday"
+            name="birthday"
+            type="date"
+            InputLabelProps={{ shrink: true }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            select
+            label="Gender"
+            name="gender"
+            defaultValue=""
+          >
+            <MenuItem value="">Select</MenuItem>
+            <MenuItem value="male">Male</MenuItem>
+            <MenuItem value="female">Female</MenuItem>
+            <MenuItem value="other">Other</MenuItem>
+            <MenuItem value="prefer_not_to_say">Prefer not to say</MenuItem>
+          </TextField>
           <TextField margin="normal" required fullWidth label="Email Address" name="email" autoComplete="email" />
           <TextField margin="normal" required fullWidth name="password" label="Password" type="password" autoComplete="new-password" />
           <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
