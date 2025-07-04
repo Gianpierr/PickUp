13b8af7a437da90e29bbfecdcfa308272937c558
Sydney Wells <smwells06@gmail.com>
2025-07-03 23:16:40 -0500

    comitting for tech exercise

--- pickup_frontend/src/pages/login.jsx
+++ pickup_frontend/src/pages/login.jsx
@@ -16,7 +16,7 @@ function Login() {
     try {
       const data = await loginAPI({ username, password });
       //  token?
-      alert('Yay, Login successful!');
+      alert('Login successful!');
     } catch (error) {
       alert('Login failed!');
     }
