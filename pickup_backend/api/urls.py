029105f82d45956c0225166b5f614b549086a947
Christian Theisen <ctheisen606@gmail.com>
2025-07-17 20:57:14 -0500

    Implement player registration and filtering endpoints

--- pickup_backend/api/urls.py
+++ pickup_backend/api/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
-    SignupView,
-    SportViewSet,
-    GameViewSet,
-    ParticipationViewSet
+    SignupView, 
+    SportViewSet, 
+    GameViewSet, 
+    ParticipationViewSet, 
+    UsersView,
+    CreateGameView,
+    RegisterView,
+    PlayerViewSet,
 )
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -15,9 +19,10 @@ router = DefaultRouter()
 router.register(r'sports', SportViewSet)
 router.register(r'games', GameViewSet)
 router.register(r'participations', ParticipationViewSet)
+router.register(r'players', PlayerViewSet)
 
 urlpatterns = [
-    path('users/', UsersView.as_view(),  name='users')
+    path('users/', UsersView.as_view(), name='users'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('', include(router.urls)),
 
@@ -25,4 +30,8 @@ urlpatterns = [
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
+
+    path('games/create/', CreateGameView.as_view(), name='create-game'),
+
+    path('register/', RegisterView.as_view(), name='register'),
 ]
